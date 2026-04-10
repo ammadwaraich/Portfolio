0d1692e2cfa1c035e151c6efce5040782bceca15
ammadwaraich <ammadwaraich99@gmail.com>
2026-04-10 19:14:08 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+const words=["Web Developer","Freelancer","Problem Solver"];
+
+let wordIndex=0;
+let charIndex=0;
+let deleting=false;
+
+const typing=document.getElementById("typing");
+
+function typeEffect(){
+
+const currentWord=words[wordIndex];
+
+if(!deleting){
+
+typing.textContent=currentWord.substring(0,charIndex+1);
+charIndex++;
+
+if(charIndex===currentWord.length){
+deleting=true;
+setTimeout(typeEffect,1200);
+return;
+}
+
+}else{
+
+typing.textContent=currentWord.substring(0,charIndex-1);
+charIndex--;
+
+if(charIndex===0){
+deleting=false;
+wordIndex=(wordIndex+1)%words.length;
+}
+
+}
+
+setTimeout(typeEffect,deleting?40:80);
+
+}
+
+typeEffect();
+
+
+function showPage(pageId){
+
+let pages=document.querySelectorAll(".page");
+
+pages.forEach(function(page){
+page.classList.remove("active");
+});
+
+let selectedPage=document.getElementById(pageId);
+
+selectedPage.classList.add("active");
+
+selectedPage.scrollIntoView({
+behavior:"smooth"
+});
+
+}
+
+window.onload=function(){
+showPage("about");
+};
